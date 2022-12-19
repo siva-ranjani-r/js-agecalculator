0f906bf76dd99b599f2c30f25042c326aa97b525
siva-ranjani-r <siva2300rs@gmail.com>
2022-12-19 10:29:04 +0530

    Adding js file

---
+++ js/cal.js
@@ -0,0 +1,8 @@
+var n=prompt("Enter the date mm-dd-yyyy")
+d1= new Date()
+present=d1.getFullYear()
+var d = new Date(n);
+year = d.getFullYear(); 
+document.write(d);
+age=present-year;
+document.write(age);
