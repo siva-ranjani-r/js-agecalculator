c654d49f79d20ff45cd7cad0d27840da9adb44b7
siva-ranjani-r <siva2300rs@gmail.com>
2022-12-19 10:50:42 +0530

    Adding js file.

--- js/cal.js
+++ js/cal.js
@@ -1,8 +1,19 @@
-var n=prompt("Enter the date mm-dd-yyyy")
-d1= new Date()
-present=d1.getFullYear()
-var d = new Date(n);
-year = d.getFullYear(); 
-document.write(d);
-age=present-year;
-document.write(age);
+var current_date=new Date();
+x=current_date.getFullYear();
+y=current_date.getMonth();
+z=current_date.getDate();
+document.write("current date :"+x+"-"+y+"-"+z+"<br>");
+
+var DOB=String(prompt("enter Date of Birth"));
+var dob=new Date(DOB);
+a=dob.getFullYear();
+b=dob.getMonth();
+c=dob.getDate();
+document.write("date of birth :"+a+"-"+b+"-"+c+"<br>");
+
+var age=x-a;
+if(b>y||b==y&&b>z)
+{
+	age--;
+}		
+document.write("entered person age is :"+age);
